4f3a51409359f514ed52b24baf4fa521da1fa5d3
Matthew Sheldon <matthewsheldon34@gmail.com>
2026-08-05 14:32:33 +0300

    updated vite.config index.html

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/pricing-component-main/',  // ← Changed from './'
+  base: './',
   plugins: [
     tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
   ],
+ 
+
 })
